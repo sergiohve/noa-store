3ef65927a995cb885070d12b0fe32520ccdb0f6e
unknown <sherrera@ad3soft.com>
2025-09-20 11:19:29 -0400

    cambios

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yihmI33I7B4Hybhlbqw7Gqq4RKhNJ+eDOQ7NeVqxx5w=\"\n}"
---
+++ .next/static/chunks/src_app_page_tsx_45a68a45._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_swiper_swiper_mjs_77e238d2._.js",
+  "static/chunks/src_065cd7e2._.js",
+  "static/chunks/node_modules_next_46d16782._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_ed5d7820._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_f6a13b47._.js",
+  "static/chunks/node_modules_4dd26d48._.js",
+  "static/chunks/node_modules_swiper_acd668f0._.css"
+],
+    source: "dynamic"
+});
