637e66092778ee73f40d086db026282238605112
unknown <sherrera@ad3soft.com>
2025-09-20 14:26:51 -0400

    cambios

---
+++ .next/static/chunks/src_app_page_tsx_0970410a._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_swiper_swiper_mjs_94e985fc._.js",
+  "static/chunks/src_af6eb9fd._.js",
+  "static/chunks/node_modules_next_d6d72620._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_ed5d7820._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_f6a13b47._.js",
+  "static/chunks/node_modules_21f311b5._.js",
+  "static/chunks/node_modules_swiper_acd668f0._.css"
+],
+    source: "dynamic"
+});
